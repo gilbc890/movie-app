c20e4b61dfd5e9749a8e040b818d981ceda09e62
Byungchan Gil <gilbc890@gmail.com>
2019-01-23 07:15:41 -0500

    test

---
+++ src/App.js
@@ -0,0 +1,34 @@
+/* eslint react/no-did-mount-set-state: 0 */
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from 'react-router-dom';
+
+import logo from './logo.jpg';
+import './App.css';
+
+import MoviesList from './MoviesList';
+import MovieDetail from './MovieDetail';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/movie-app" component={MoviesList} />
+        <Route path="movie-app/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+
+export default App;
+
