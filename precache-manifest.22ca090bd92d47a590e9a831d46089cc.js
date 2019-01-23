d9bf586af19739f81dd839a3aca6fbf541ffe59e
Byungchan Gil <gilbc890@gmail.com>
2019-01-23 06:02:41 -0500

    Updates

---
+++ precache-manifest.22ca090bd92d47a590e9a831d46089cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "de32a81ade48dd180ddf",
+    "url": "/movie-app/static/css/main.d6846fc9.chunk.css"
+  },
+  {
+    "revision": "de32a81ade48dd180ddf",
+    "url": "/movie-app/static/js/main.de32a81a.chunk.js"
+  },
+  {
+    "revision": "0f952a66608f465e82be",
+    "url": "/movie-app/static/js/1.0f952a66.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "536a472d2b677f414400422cd2ec23d4",
+    "url": "/movie-app/static/media/logo.536a472d.jpg"
+  },
+  {
+    "revision": "6a0f3ea2c6a1024db79a797bcf66e871",
+    "url": "/movie-app/index.html"
+  }
+];
